8229289272735a46fbdff8d37c8339fb5e5db809
sahrul223344 <151334819+sahrul223344@users.noreply.github.com>
2024-01-01 21:51:44 +0700

    Add files via upload

---
+++ page.js
@@ -0,0 +1,10 @@
+const Page =({ params }) => {
+    console.log(params)
+    return(
+        <>
+           <h1>SEARCH PAGE</h1>
+           <P>INI ADALAH SEARCH PAGE</P>
+        </>
+    )
+}
+export default Page
